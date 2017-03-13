f42ebc6c1f1bfe500df0936b7b28e81137c65a0d
May <calopc.canova@gmail.com>
2017-03-13 09:06:10 +0700

    finish hw2 explained

--- src/main.js
+++ src/main.js
@@ -1,22 +1,8 @@
-// // The Vue build version to load with the `import` command
-// // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-// import Vue from 'vue'
-// import App from './App'
-// import router from './router'
-//
-// Vue.config.productionTip = false
-//
-// /* eslint-disable no-new */
-// new Vue({
-//   el: '#app',
-//   router,
-//   template: '<App/>',
-//   components: { App }
-// })
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// ---------------------
+import store from './store'
+
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
 
@@ -24,7 +10,9 @@ Vue.use(VueMaterial)
 
 import Axios from 'axios'
 Vue.$http = Axios
-// ---------------------
+Axios.defaults.baseURL = 'https://iccs340-vue-api-auth.herokuapp.com/'
+Axios.defaults.headers.common.Accept = 'application/json'
+Axios.defaults.withCredentials = true
 
 Vue.config.productionTip = false
 
@@ -32,6 +20,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
